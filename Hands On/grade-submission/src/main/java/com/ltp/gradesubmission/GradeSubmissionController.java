3df7c9e20a4d408063556dc893a4343b35c439ff
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2023-12-08 23:44:55 +0530

    Added a Controller Class with a sample method of GetMapping

---
+++ Hands On/grade-submission/src/main/java/com/ltp/gradesubmission/GradeSubmissionController.java
@@ -0,0 +1,17 @@
+package com.ltp.gradesubmission;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class GradeSubmissionController {
+
+    @GetMapping("/grades")
+    public String getMethodName() {
+        System.out.println("Entered getMethodName method");
+        return "grades";
+    }
+    
+
+}
