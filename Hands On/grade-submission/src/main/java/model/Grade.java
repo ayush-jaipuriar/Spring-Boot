16b751b9c7d3c6724e59cff5dcd4d580a65e02ee
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2023-12-10 15:37:38 +0530

    Added Grade POJO

---
+++ Hands On/grade-submission/src/main/java/model/Grade.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Grade {
+    private String name;
+    private String subject;
+    private String score;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubject() {
+        return this.subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getScore() {
+        return this.score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+    
+
+    public Grade(String name, String subject, String score) {
+        this.name = name;
+        this.subject = subject;
+        this.score = score;
+    }
+    
+
+}
