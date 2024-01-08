08915833a3ce0804499f73a36ff83f0f0d1dd466
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-01-08 23:33:30 +0530

    Added GradeRepository Mcok and Injected the mock to GradeService

---
+++ Testing/grade-submission/src/test/java/com/ltp/gradesubmission/GradeServiceTest.java
@@ -0,0 +1,20 @@
+package com.ltp.gradesubmission;
+
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.ltp.gradesubmission.repository.GradeRepository;
+import com.ltp.gradesubmission.service.GradeService;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GradeServiceTest {
+    
+    @Mock
+    GradeRepository gradeRepository;
+
+    @InjectMocks
+    GradeService gradeService;
+
+}
