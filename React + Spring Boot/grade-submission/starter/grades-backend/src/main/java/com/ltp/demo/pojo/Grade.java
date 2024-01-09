e1f8a3303a9c686fc7f5cdc511b689c9133ab03f
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-01-09 21:10:40 +0530

    Server Side Starter Code for React + Springboot

--- React + Spring Boot/grade-submission/starter/grades-backend/src/main/java/com/ltp/demo/pojo/Grade.java
+++ React + Spring Boot/grade-submission/starter/grades-backend/src/main/java/com/ltp/demo/pojo/Grade.java
@@ -1,25 +1,11 @@
-package com.ltp.gradesubmission.pojo;
-
-import java.util.UUID;
-
-import javax.validation.constraints.NotBlank;
-
-import com.ltp.gradesubmission.validation.Score;
+package com.ltp.demo.pojo;
 
 public class Grade {
 
-    @NotBlank(message = "Name cannot be blank")
     private String name;
-    @NotBlank(message = "Subject cannot be blank")
     private String subject;
-    @Score(message = "Score must be a letter grade")
     private String score;
-    private String id;
 
-
-    public Grade() {
-        this.id = UUID.randomUUID().toString();
-    }
 
     public Grade(String name, String subject, String score) {
         this.name = name;
@@ -27,8 +13,13 @@ public class Grade {
         this.score = score;
     }
 
+
+    public Grade() {
+    }
+
+
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public void setName(String name) {
@@ -36,7 +27,7 @@ public class Grade {
     }
 
     public String getSubject() {
-        return subject;
+        return this.subject;
     }
 
     public void setSubject(String subject) {
@@ -44,20 +35,11 @@ public class Grade {
     }
 
     public String getScore() {
-        return score;
+        return this.score;
     }
 
     public void setScore(String score) {
         this.score = score;
-    }
-
-
-    public String getId() {
-        return this.id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
 }
--- Testing/grade-submission/src/test/java/com/ltp/gradesubmission/GradeServiceTest.java
+++ Testing/grade-submission/src/test/java/com/ltp/gradesubmission/GradeServiceTest.java
@@ -1,10 +1,17 @@
 package com.ltp.gradesubmission;
 
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.assertj.core.util.Arrays;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import com.ltp.gradesubmission.pojo.Grade;
 import com.ltp.gradesubmission.repository.GradeRepository;
 import com.ltp.gradesubmission.service.GradeService;
 
@@ -16,5 +23,15 @@ public class GradeServiceTest {
 
     @InjectMocks
     GradeService gradeService;
+
+    @Test
+    public void getGradesTest() {   
+        // Grade grade = new Grade("Ayush", "A+", "Computer");
+        // List<Grade> gradeList = Arrays.asList(grade);
+        // when(gradeRepository.getGrades()).thenReturn(gradeList);
+        
+        
+    }
+
 
 }
