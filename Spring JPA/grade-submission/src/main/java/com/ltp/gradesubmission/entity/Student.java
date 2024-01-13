a74ecb54675ad6c78890c605a7ca72087682bc52
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-01-14 00:04:47 +0530

    Added Student entity

--- Spring JPA/grade-submission/src/main/java/com/ltp/gradesubmission/entity/Student.java
+++ Spring JPA/grade-submission/src/main/java/com/ltp/gradesubmission/entity/Student.java
@@ -2,10 +2,22 @@ package com.ltp.gradesubmission.entity;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "student")
 public class Student {
 
+    @Column(name = "id")
     private Long id;
+    
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "birthdate")
     private LocalDate birthDate;
 
     public Long getId() {
