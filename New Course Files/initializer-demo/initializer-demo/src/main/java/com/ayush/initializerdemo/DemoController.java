2e490266adf7b305d189fad4814381fb17e85281
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-03-30 22:17:51 +0530

    Add Rest Controller and Get mapping for 'Workout' endpoint

--- New Course Files/initializer-demo/initializer-demo/src/main/java/com/ayush/initializerdemo/DemoController.java
+++ New Course Files/initializer-demo/initializer-demo/src/main/java/com/ayush/initializerdemo/DemoController.java
@@ -1,18 +1,20 @@
 package com.ayush.initializerdemo;
 
-import org.springframework.stereotype.Controller;
+// Import necessary annotations and classes for Spring Boot web application development
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
+// Define the DemoController class as a REST controller
 @RestController
 public class DemoController {
 
+    // Define a GET mapping for the "/workout" endpoint
     @GetMapping("/workout")
-    public String getMethodName() {
-        return new String("Lets run a 5k marathon");
+    // Method to handle requests to the "/workout" endpoint
+    // It retrieves the student's name from the application properties and returns it as a response
+    public String getMethodName(@Value("${student.name}") String name) {
+        // Return the student's name as a string
+        return new String(name);
     }
-    
-
 }
