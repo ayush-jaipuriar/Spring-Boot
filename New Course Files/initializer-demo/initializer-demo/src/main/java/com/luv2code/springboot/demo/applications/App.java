970ce35ea31164d377c365a39d1fa996d5218f3d
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-03-31 17:59:12 +0530

    Add Spring JPA and MySQL Connector dependencies to pom.xml

--- New Course Files/initializer-demo/initializer-demo/src/main/java/com/luv2code/springboot/demo/applications/App.java
+++ New Course Files/initializer-demo/initializer-demo/src/main/java/com/luv2code/springboot/demo/applications/App.java
@@ -10,8 +10,8 @@ import com.ayush.service.LazyService;
 @SpringBootApplication
 public class App {
 
-	public static void main(String[] args) {
-		// Run the Spring application and get the application context
+    public static void main(String[] args) {
+        // Run the Spring application and get the application context
         ApplicationContext context = SpringApplication.run(App.class, args);
 
         // Retrieve the bean from the Spring container
@@ -21,13 +21,14 @@ public class App {
         System.out.println(coach.getDailyWorkout());
         System.out.println(((CricketCoach) coach).getDailyFortune());
 
-		coach = context.getBean("swimmingCoach", SwimmingCoach.class);
-		System.out.println(coach.getDailyWorkout());
-		System.out.println(((SwimmingCoach) coach).getCoachInfo());
+        coach = context.getBean("swimmingCoach", SwimmingCoach.class);
+        System.out.println(coach.getDailyWorkout());
+        System.out.println(((SwimmingCoach) coach).getCoachInfo());
 
-        // Since the LazyService class is marked as @Lazy, it will get initialized now instead of during Spring initialization like other beans
+        // Since the LazyService class is marked as @Lazy, it will get initialized now
+        // instead of during Spring initialization like other beans
         FortuneService service = context.getBean("lazyService", LazyService.class);
         ((LazyService) service).doSomething();
-	}
+    }
 
 }
