f4787be7193bc5edb0980cf0305908d60da5a2ea
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-03-31 10:48:10 +0530

    Update the InitializerDemoApplication to remove commented-out code and streamline the main method

--- New Course Files/initializer-demo/initializer-demo/src/main/java/com/luv2code/springboot/demo/applications/MycoolappApplication.java
+++ New Course Files/initializer-demo/initializer-demo/src/main/java/com/luv2code/springboot/demo/applications/MycoolappApplication.java
@@ -2,12 +2,21 @@ package com.luv2code.springboot.demo.applications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MycoolappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MycoolappApplication.class, args);
+		// Run the Spring application and get the application context
+        ApplicationContext context = SpringApplication.run(MycoolappApplication.class, args);
+
+        // Retrieve the bean from the Spring container
+        Coach coach = context.getBean("cricketCoach", Coach.class);
+
+        // Use the bean
+        System.out.println(coach.getDailyWorkout());
+        System.out.println(((CricketCoach) coach).getDailyFortune());
 	}
 
 }
