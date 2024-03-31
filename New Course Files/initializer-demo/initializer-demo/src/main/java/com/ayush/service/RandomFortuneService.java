c988b8600af435fec36c6bb9085c0ee8ff9e9b4d
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-03-31 10:39:24 +0530

    Add RabdomFortuneService class, an implementation of FortuneService which sends random fortunes

---
+++ New Course Files/initializer-demo/initializer-demo/src/main/java/com/ayush/service/RandomFortuneService.java
@@ -0,0 +1,22 @@
+package com.ayush.service;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RandomFortuneService implements FortuneService {
+    private String[] fortunes = {
+        "Today is your lucky day!",
+        "You will have a great day!",
+        "Good luck is coming your way!"
+    };
+
+    private Random random = new Random();
+
+    @Override
+    public String getFortune() {
+        int randomIndex = random.nextInt(fortunes.length);
+        return fortunes[randomIndex];
+    }
+}
