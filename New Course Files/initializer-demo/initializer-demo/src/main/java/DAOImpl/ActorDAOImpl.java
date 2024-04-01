39fc93cf72091806499d049454f296442f562dfd
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-04-01 23:53:34 +0530

    Refactor ActorDAOImpl to use constructor injection and add transactional annotation

--- New Course Files/initializer-demo/initializer-demo/src/main/java/DAOImpl/ActorDAOImpl.java
+++ New Course Files/initializer-demo/initializer-demo/src/main/java/DAOImpl/ActorDAOImpl.java
@@ -1,17 +1,25 @@
 package DAOImpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import DAO.ActorDAO;
 import entity.Actor;
 import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 
 @Repository
 public class ActorDAOImpl implements ActorDAO{
     private EntityManager entityManager;
 
+    @Autowired
+    public ActorDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
 
     @Override
+    @Transactional
     public void save(Actor actor) {
         entityManager.persist(actor);
     }
